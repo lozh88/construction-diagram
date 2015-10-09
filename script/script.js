d6e7901ff200c6293e3f288967be478af4419722
Lauren Henry <lauren.kitson88@gmail.com>
2015-10-09 17:06:48 +0100

    clickable divs affect text
    other: tried to create pop up box with .popped, moved background-color
    to each div

--- script/script.js
+++ script/script.js
@@ -7,5 +7,33 @@ $(document).ready(function() {
 		$(this).css('transform', 'scale(1)');
 	});
 
+	$('#div-one').on('click', function() {
+		$('#title').text("Domestic Wall Envelope");
+		$('.tell-me').text("A wall is fascinating. Did you know an average external wall in the UK is 40cm thick?");
+	})
 
+	$('#div-two').on('click', function() {
+		$('#title').text("Pavement Envelope");
+		$('.tell-me').text("Funny thing... the pavement is actually the road, not the walk way.");
+	})
+
+	$('#div-three').on('click', function() {
+		$('#title').text("Slab Envelope");
+		$('.tell-me').text("The slab that we're talking about is the cast or insitu concrete you walk on in commerial buildings");
+	})
+
+	$('#div-four').on('click', function() {
+		$('#title').text("Roof Truss");
+		$('.tell-me').text("Ok, so this isn't strictly an envelope but it is very interesting...promise");
+	})
+
+	$('#div-five').on('click', function() {
+		$('#title').text("Undefined");
+		$('.tell-me').text("Undefined");
+	})
+
+	$('.play')on('click', function() {
+		$('#pop-up').addClass('popped');
+		});
+	})
 });
