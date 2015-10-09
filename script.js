e09af60bdc586981ee6bbe8626808265d430179c
Lauren Henry <lauren.kitson88@gmail.com>
2015-10-09 12:06:42 +0100

    alternated bubble animation
    added classes .mover and .shaker

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
-	var bubble = $('.bubble');
 	
+	var mover = $('.mover');
+	var shaker = $('.shaker')
+
 	function runIt() {
-		bubble.animate({top: '+=20px'}, 1000);
-		bubble.animate({top: '-=20px'}, 1000, runIt);
+		mover.animate({top: '+=20px'}, 1000);
+		mover.animate({top: '-=20px'}, 1000);
+		shaker.animate({top: '-=10px'}, 1125);
+		shaker.animate({top: '+=10px'}, 1125, runIt);
 	}
+
+	// $('.bubble').on("mouseenter", function() {
+	// 	$('.bubble').css("background-color": "red")
+	// 	});
+	// })
 
 	runIt();
 });
+	
+
